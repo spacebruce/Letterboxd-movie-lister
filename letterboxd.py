fcef928b7c11f2a42f4d6c487edaf174e5937563
bakagamedev <noel.abercrombie@hotmail.com>
2018-11-14 16:25:05 +0000

    misc style and consistency fixes

--- letterboxd.py
+++ letterboxd.py
@@ -27,22 +27,22 @@ class Film():
 	def Stars(self):
 		star = '★'
 		half = '½'
-		ratinghalf = int(self.rating / 2)
-		returnstring = ''
-		for i in range(0, ratinghalf):
-			returnstring += star
-		if(ratinghalf * 2 != self.rating):
-			returnstring += half
-		return returnstring
+		ratingHalf = int(self.rating / 2)
+		starRating = ''
+		for i in range(0, ratingHalf):
+			starRating += star
+		if(ratingHalf * 2 != self.rating):
+			starRating += half
+		return starRating
 		
 # Parameters
-userName = ""
-outPath = ""
+userName = ''
+outPath = ''
 yearMode = False
 
 # Argument handling
-#		0				1		2		3
-#python letterboxd.py username outpath year?
+#		 0			   1		2		3
+# python letterboxd.py username outpath year?
 ArgCount = len(sys.argv)
 
 if(ArgCount >= 2):
@@ -68,25 +68,25 @@ if(yearMode):
 		pageYear.year = i
 		pageList.append(pageYear)
 else:
-	firstPage = Page('https://letterboxd.com/' + userName + '/films/page/1/')	#open first page and read pagination section
+	firstPage = Page('https://letterboxd.com/' + userName + '/films/page/1/')	# Open first page and read pagination section
 	firstPage.Load()
 	pageList.append(firstPage)
 	
-	pageDiscovery = firstPage.soup.find(class_='paginate-pages')	#find links in pagination section
+	pageDiscovery = firstPage.soup.find(class_='paginate-pages')	# Find links in pagination section
 	pageDiscoveryList = pageDiscovery.find_all('a')
 	
-	#find last page number
+	# find last page number
 	pageCount = 0
 	for pageID in pageDiscoveryList:		
 		pageNumber = pageID.contents[0]
 	pageCount = max(pageCount, int(pageNumber))
 	
-	#add range to search list
+	# add range to search list
 	for pageNum in range(2, pageCount + 1):
 		pageTemp = Page('https://letterboxd.com/' + userName + '/films/page/' + str(pageNum) + '/')
 		pageList.append(pageTemp)
 
-#find films on pages
+# find films on pages
 filmList = []
 
 print('Reading...')
@@ -99,8 +99,8 @@ for i in range(0, len(pageList)):
 	# read posters
 	posterContainer = page.soup.find(class_='poster-list')
 	if posterContainer:
-		ratingList = posterContainer.find_all('li')		#	<li class="poster-container" data-owner-rating="0">
-		nameList = posterContainer.find_all('img')		#	<img alt="John Wick: Chapter 2" class="image" height="105" src="https://s1.ltrbxd.com/static/img/empty-poster-70.8461d4ea.png" width="70"/>
+		ratingList = posterContainer.find_all('li')		# <li class="poster-container" data-owner-rating="0">
+		nameList = posterContainer.find_all('img')		# <img alt="John Wick: Chapter 2" class="image" height="105" src="https://s1.ltrbxd.com/static/img/empty-poster-70.8461d4ea.png" width="70"/>
 		
 		for film in range(0, len(nameList)):
 			nameEntry = nameList[film]
@@ -115,7 +115,8 @@ for i in range(0, len(pageList)):
 			
 			filmList.append(Film(name, rating, year))
 
-	print(str(i + 1) + "/" + str(len(pageList)) + " " + page.url + " (" + str(len(filmList)) + " films discovered)")
+	print(str(i + 1) + '/' + str(len(pageList)) + ' ' + page.url + ' (' + str(len(filmList)) + ' films discovered)')
+
 # sort list alphabetically
 filmList = sorted(filmList, key=lambda film: film.name)
 
